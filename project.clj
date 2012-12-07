ea5a8ef2b26cb0ba1e9286d495582ffc10aa19ae
Jeff Weiss <jweiss@redhat.com>
2012-12-07 09:23:54 -0500

    Make trees more editable.  Do a bit of cleanup.  Bump version.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject ui.navigate "0.1.0"
+(defproject ui.navigate "0.2.0"
   :description "A small library to help automated tests navigate a hierarchical user interface"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.zip "0.1.1"]])
--- src/ui/navigate.clj
+++ src/ui/navigate.clj
@@ -1,9 +1,11 @@
 (ns ui.navigate
   (:require [clojure.zip :as zip]
+            [clojure.data.zip :as zf]
             [clojure.set])
   (:import [java.util NoSuchElementException]))
 
-(defmacro nav-tree "takes a nested vector structure and returns nested maps" [[page-id args form & links]]
+(defmacro nav-tree "takes a nested vector structure and returns nested maps"
+  [[page-id args form & links]]
   (merge `{:page ~page-id
            :fn (fn ~args ~form)
            :req-args ~(vec (map keyword args))}
@@ -13,23 +15,32 @@
            {})))
 
 (defn page-zip [tree] (zip/zipper (constantly true)
-                          #(:links %)
-                          #(conj %1 {:links %2})
-                          tree))
+                                  #(:links %)
+                                  #(conj %1 {:links %2})
+                                  tree))
 
-(defn find-node [z pred]
+(defn matches-page
+  "Returns predicate for a loc to match a page named page."
+  [page]
+  (fn [loc]
+    (= (-> loc zip/node :page)
+       page)))
+
+(defn find-node "Finds first node in z that matches pred."
+  [z pred]
   (and z (->> (iterate zip/next z)
-              (take-while #(not (zip/end? %1)))
-              (some  #(when (pred (zip/node %)) %)))))
+            (take-while #(not (zip/end? %1)))
+            (some  #(when (pred %) %)))))
 
-(defn page-path [page z]
-  (let [page-match? #(= (:page %) page)
-        first-match (find-node z page-match?)]
-    (if (nil? first-match)
-      (throw (NoSuchElementException. (str "Page " page " was not found in navigation tree."))))
+(defn page-path
+  "Returns a list, the path from the root of zipper tree z to page."
+  [page z]
+  (let [first-match (or (find-node z (matches-page page))
+                        (throw (NoSuchElementException.
+                                (str "Page " page " was not found in navigation tree."))))]
     (conj (zip/path first-match) (zip/node first-match))))
 
-(defn navigate
+(defn navigate 
   ([page z args]
      (let [path (page-path page z)
            all-req-args (set (mapcat :req-args path))
@@ -44,11 +55,25 @@
 
 (defn nav-fn
   "Closes over a page zip structure and returns a navigation function.
-  tree-zip-var should be a var containing a page-zip zipper structure.
-  A var is used here so you can update the page structure during
-  development, without having to call this function again."
-  [tree-zip-var]
+  nav-tree-ref should be an IDeref (var, atom, etc) containing a
+  page-zip zipper structure. deref is used here so you can update the
+  page structure during development, without having to call this
+  function again."
+  [nav-tree-ref]
   (fn 
     ([page args]
-       (navigate page (var-get tree-zip-var) args))
-    ([page] (navigate page (var-get tree-zip-var) {}))))
+       (navigate page (page-zip (deref nav-tree-ref)) args))
+    ([page] (navigate page (page-zip (deref nav-tree-ref)) {}))))
+
+(defn add-subnav
+  "In nav-tree tree, add subnavigation branch as a child of parent
+  page. If branch is already present, replaces that branch."
+  [tree parent-page branch]
+  (let [parent-node (find-node (page-zip tree) (matches-page parent-page))]
+    (assert parent-node (format "Graft point %s not found in tree." parent-page))
+    (if-let [existing-child-loc (->> parent-node
+                                   zf/children
+                                   (filter (matches-page (:page branch)))
+                                   first)]
+      (-> existing-child-loc (zip/replace branch) zip/root)
+      (-> parent-node (zip/append-child branch) zip/root))))
