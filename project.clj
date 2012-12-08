6a384720cc1a56f05b4f8f598cbd458559c13e52
Jeff Weiss <jweiss@redhat.com>
2012-12-08 14:12:02 -0500

    Inc version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ui.navigate "0.2.0"
+(defproject ui.navigate "0.2.1-SNAPSHOT"
   :description "A small library to help automated tests navigate a hierarchical user interface"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.zip "0.1.1"]])
